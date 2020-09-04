32fe4df99a7517c585d093558ff72019580dea19
adrianschiop <10593574+adrianschiop@users.noreply.github.com>
2020-09-04 18:24:21 +0300

    Added sequelize migrations

---
+++ migrations/20200904145139-create-todo.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', // name of Target model
+          key: 'id', // Key in Target model that we're referencing
+        },
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      completed: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        default: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Todos');
+  }
+};
