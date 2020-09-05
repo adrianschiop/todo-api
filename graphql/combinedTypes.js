88849453f1e28eb19a92f88e829467bec6c86081
adrianschiop <10593574+adrianschiop@users.noreply.github.com>
2020-09-05 10:39:55 +0300

    Fixed boolean error from updateTodo mutation

--- graphql/combinedTypes.js
+++ graphql/combinedTypes.js
@@ -8,6 +8,7 @@ const queryTypes = gql`
 
   scalar Password
   scalar Email
+  scalar Boolean
 
   type Query {
     _: Boolean
--- graphql/scalars/index.js
+++ graphql/scalars/index.js
@@ -3,7 +3,7 @@ import { GraphQLScalarType } from 'graphql';
 const customScalars = {
   Password: new GraphQLScalarType({
     name: 'Password',
-    description: 'Password scalar types',
+    description: 'Password scalar type',
     parseValue(value) { // value from the client
       if (!value || value.length < 6)
         throw new Error("Password must have minimum 6 characters");
@@ -13,12 +13,19 @@ const customScalars = {
   }),
   Email: new GraphQLScalarType({
     name: "Email",
-    description: 'Email scalar types',
+    description: 'Email scalar type',
     parseValue(value) { // value from the client
       if (!/[a-z0-9._%-]+@[a-z0-9._%-]+\.[a-z]{2,4}/.test(value))
         throw new Error("Email is not valid");
 
       return value;
+    }
+  }),
+  Boolean: new GraphQLScalarType({
+    name: "Boolean",
+    description: "Boolean scalar type",
+    parseValue(value) { // value from the client
+      return Boolean(value);
     }
   })
 };
